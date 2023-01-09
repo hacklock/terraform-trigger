fe8fdc14e85fe956d286960ee1a6dc0a93c3aac2
Wilton R. Cruz <piumhi10@gmail.com>
2023-01-09 10:22:37 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,6 +4,20 @@ terraform {
       source = "hashicorp/google"
     }
   }
+  
+  resource "random_id" "bucket_prefix" {
+  byte_length = 8
+}
+
+resource "google_storage_bucket" "default" {
+  name          = "${random_id.bucket_prefix.hex}-bucket-tfstate"
+  force_destroy = false
+  location      = "US"
+  storage_class = "STANDARD"
+  versioning {
+    enabled = true
+  }
+}
 
    backend "gcs" {
     bucket  = "piumhi10terraform"
