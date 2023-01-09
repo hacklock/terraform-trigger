8d132454fea1e669cda60bf851a0e482f37aaae4
Wilton R. Cruz <piumhi10@gmail.com>
2023-01-09 09:58:26 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,14 +6,14 @@ terraform {
   }
 
    backend "gcs" {
-    bucket  = "barberoterraform"
+    bucket  = "piumhi10terraform"
     prefix  = "terraform/state"
   }
   
 }
 
 provider "google" {
-  project = "barbero-devops-iac"
+  project = "piumhi10-devops-iac"
   region  = "us-central1"
   zone    = "us-central1-c"
 }
